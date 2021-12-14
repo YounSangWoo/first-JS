90896b3866046f9acc2a92828129fe3a0a91b870
YounSangWoo <sangwoo@DESKTOP-BQDTVSB.localdomain>
2021-12-14 14:55:20 +0900

    js object

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const a = 5;  // 변하면 안되는 변수 
-let myName = "SW"; //카멜케이스 변수명 자성법, let은 변경 가능한 변수 선언
-console.log(a + 2);
-console.log("hello " + myName)
+const player = {
+    name: "SW",
+    points: 10,
+    fat: true,
+};
+console.log(player);
 
-myName = "SangWoo" //let은 처음 변수를 생성할때만 사용
-console.log("Your new name is "+myName)
+player.lastname = "YOUN"
+console.log(player);
