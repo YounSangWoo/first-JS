b5a56e6e3c85ba478d8798644cc7081fa42d14a6
YounSangWoo <sangwoo@DESKTOP-BQDTVSB.localdomain>
2021-12-14 14:09:19 +0900

    자바스크립트 변수const ,let

---
+++ app.js
@@ -0,0 +1,7 @@
+const a = 5;  // 변하면 안되는 변수 
+let myName = "SW"; //카멜케이스 변수명 자성법, let은 변경 가능한 변수 선언
+console.log(a + 2);
+console.log("hello " + myName)
+
+myName = "SangWoo" //let은 처음 변수를 생성할때만 사용
+console.log("Your new name is "+myName)
