f3771d1043db867bc3aa8caa3365b3d677ed8317
YounSangWoo <sangwoo@DESKTOP-BQDTVSB.localdomain>
2021-12-17 21:57:01 +0900

    toggle

--- app.js
+++ app.js
@@ -1,40 +1,7 @@
 const h1 = document.querySelector(".hello:first-child h1");
 console.dir(h1);
 function handleTitleClick() {
-    h1.style.color = "blue";
-    
+    h1.classList.toggle(clicked)
 }
 
-function handleMouseEnter() {
-    h1.innerText = ("mouse is here");
-}
-
-function handleMouseLeave() {
-    h1.innerText = ("mouse is gone");
-}
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-    alert("copier!!");
-}
-
-
-function handleWindowOffline() {
-    alert("wifi offline");
-}
-
-function handleWindowOnline() {
-    alert("ALL GOOD");
-}
-
-h1.onclick = handleTitleClick;
-h1.addEventListener("mouseenter", handleMouseEnter)
-h1.addEventListener("mouseleave", handleMouseLeave)
-
-window.addEventListener("resize", handleWindowResize)
-window.addEventListener("copy", handleWindowCopy)
-
-window.addEventListener("offline", handleWindowOffline)
-window.addEventListener("online", handleWindowOnline)
+h1.addEventListener("click", handleTitleClick);
